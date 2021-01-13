cc1e7137bf3543b52f09863fedd6eb0e0c0dee94
leagleag <leagleagleag@gmail.com>
2021-01-13 09:00:47 +0000

    Added a error message to handle id or columns name user errors

--- association_analysis/utils.py
+++ association_analysis/utils.py
@@ -2,9 +2,18 @@
 
 import pandas as pd
 import yaml
+import sys
 
 def json_to_df_with_columns(data_json, columns_list):
-    return pd.json_normalize(data_json).loc[:,columns_list]
+    try:
+        return pd.json_normalize(data_json).loc[:,columns_list]
+    except KeyError as e:
+        print("""One or more {0} could not be found in json data {1}.
+        Probable causes:
+        - the id is not a valid target or disease id
+        - one or more column names do not exist in the data
+        """.format(columns_list, data_json))
+        raise
 
 def statistics_with_columns(data_series, columns_list):
     return data_series.describe()[columns_list]
