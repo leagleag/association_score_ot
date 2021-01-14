83453e43d10057e73ca7d254842b8be1b0a73017
leagleag <leagleagleag@gmail.om>
2021-01-14 01:53:05 +0000

    ran black

--- tests/test_query_api.py
+++ tests/test_query_api.py
@@ -13,13 +13,26 @@ api_url_base = "https://platform-api.opentargets.io/v3/platform"
 # endpoint to use for association analysis
 endpoint = "/public/association/filter"
 
+
 @pytest.mark.parametrize(
     "url_endpoint,payload,expected_response",
     [
-        ("{0}{1}".format(api_url_base, endpoint), {"target": ["ENSG00000197386"], "size": 1}, (200, 1, "application/json")),
-        ("{0}{1}".format(api_url_base, endpoint), {"disease": ["Orphanet_399"], "size": 1}, (200, 1, "application/json")),
-        ("{0}{1}".format(api_url_base, endpoint), {"target": ["Orphanet_oh_no"], "size": 1}, (200, 0, "application/json")),
-    ]
+        (
+            "{0}{1}".format(api_url_base, endpoint),
+            {"target": ["ENSG00000197386"], "size": 1},
+            (200, 1, "application/json"),
+        ),
+        (
+            "{0}{1}".format(api_url_base, endpoint),
+            {"disease": ["Orphanet_399"], "size": 1},
+            (200, 1, "application/json"),
+        ),
+        (
+            "{0}{1}".format(api_url_base, endpoint),
+            {"target": ["Orphanet_oh_no"], "size": 1},
+            (200, 0, "application/json"),
+        ),
+    ],
 )
 def test_post_single_payload(url_endpoint, payload, expected_response):
     response = requests.post(url_endpoint, json=payload)
@@ -27,7 +40,9 @@ def test_post_single_payload(url_endpoint, payload, expected_response):
     assert (
         response.status_code,
         len(response_body["data"]),
-        response.headers["Content-Type"]) == expected_response
+        response.headers["Content-Type"],
+    ) == expected_response
+
 
 @responses.activate
 def test_post_single_payload_when_response_is_ok():
@@ -38,8 +53,11 @@ def test_post_single_payload_when_response_is_ok():
         json=expected_dict,
         status=200,
     )
-    data_dict = query_api.post_single_payload("https://www.host.com/fake/base/url", "/fake/endpoint", "s")
+    data_dict = query_api.post_single_payload(
+        "https://www.host.com/fake/base/url", "/fake/endpoint", "s"
+    )
     TestCase().assertDictEqual(data_dict, expected_dict)
+
 
 @responses.activate
 def test_post_single_payload_when_response_is_not_ok():
@@ -50,26 +68,42 @@ def test_post_single_payload_when_response_is_not_ok():
         json=expected_dict,
         status=400,
     )
-    data_dict = query_api.post_single_payload("https://www.host.com/fake/base/url", "/fake/endpoint", "s")
+    data_dict = query_api.post_single_payload(
+        "https://www.host.com/fake/base/url", "/fake/endpoint", "s"
+    )
     assert data_dict is None
+
 
 @pytest.mark.parametrize(
     "data_dict,expected",
     [
         ({"data": "OMG"}, ({"data": "OMG"},)),
-        ({"data": "OMG", "next": "here"}, ({"data": "OMG", "next": "here"},{"data": "OMG", "next": "here"}))
-    ]
+        (
+            {"data": "OMG", "next": "here"},
+            ({"data": "OMG", "next": "here"}, {"data": "OMG", "next": "here"}),
+        ),
+    ],
 )
 def test_post_payload_generator(data_dict, expected):
-    with patch("association_analysis.query_api.post_single_payload", return_value=data_dict):
-        g = query_api.post_payload_generator("/fake/base/url", "/fake/endpoint", {"dummy": "payload"})
+    with patch(
+        "association_analysis.query_api.post_single_payload", return_value=data_dict
+    ):
+        g = query_api.post_payload_generator(
+            "/fake/base/url", "/fake/endpoint", {"dummy": "payload"}
+        )
         assert tuple(itertools.islice(g, 2)) == expected
+
 
 def test_query_data_into_df_with_columns():
     expected_df = pd.DataFrame(index=[0, 0], data=[1, 2], columns=["column_i_want"])
     with patch(
         "association_analysis.query_api.post_payload_generator",
-        return_value=[{"data": {"column_i_want":1, "no":42}}, {"data": {"column_i_want":2, "yes":42}}]
+        return_value=[
+            {"data": {"column_i_want": 1, "no": 42}},
+            {"data": {"column_i_want": 2, "yes": 42}},
+        ],
     ):
-        data_df = query_api.query_data_into_df_with_columns("/url/base", "/endpoint", {"some": "payload"}, ["column_i_want"])
+        data_df = query_api.query_data_into_df_with_columns(
+            "/url/base", "/endpoint", {"some": "payload"}, ["column_i_want"]
+        )
     pd.testing.assert_frame_equal(expected_df, data_df)
